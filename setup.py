48897206d7bc215e54e30f3567b47b1abd7baf96
Beck <by66666@gmail.com>
2017-09-11 22:07:03 +0800

    first try

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='amplot',
+    packages=['amplot'],
+    version='0.1',
+    description='A map plot based on Amap(高德地图).',
+    author='Yang Bai',
+    author_email='by66666@gmail.com',
+    url='https://github.com/KECB/amplot',
+    download_url='https://github.com/KECB/amplot/archive/0.1.tar.gz',
+    keywords=['amplot', 'map', 'plot'],
+    classifiers=[],
+)
